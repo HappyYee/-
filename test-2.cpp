fc5f120a1b192e4d579508e4359dd95a78278036
AC_Yee <121917129+HappyYee@users.noreply.github.com>
2023-04-30 16:38:35 +0800

    Add files via upload
    // 没关cin, cout 时间为 86ms
    // 关了之后 时间为 80ms

---
+++ test-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", a + b);
+
+	return 0;
+}
+// 没关cin, cout 时间为 86ms
+// 关了之后 时间为 80ms
